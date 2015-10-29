6661f70abdaca3d67faa3312c24d9b2a250181ea
James Conkling <james.lane.conkling@gmail.com>
2015-10-28 19:18:56 -0700

    update events schema

--- initDb.sql
+++ initDb.sql
@@ -1,63 +1,63 @@
 CREATE EXTENSION IF NOT EXISTS PostGIS;
 
 CREATE TABLE events (
-  GLOBALEVENTID bigint PRIMARY KEY,
+  GLOBALEVENTID bigint PRIMARY KEY ,
   SQLDATE date ,
-  MonthYear varchar(6) ,
-  Year varchar(4) ,
+  MonthYear text ,
+  Year text ,
   FractionDate real ,
-  Actor1Code varchar(20) , --char(3)
-  Actor1Name varchar(255) ,
-  Actor1CountryCode varchar(3) ,
-  Actor1KnownGroupCode varchar(3) ,
-  Actor1EthnicCode varchar(3) ,
-  Actor1Religion1Code varchar(3) ,
-  Actor1Religion2Code varchar(3) ,
-  Actor1Type1Code varchar(3) ,
-  Actor1Type2Code varchar(3) ,
-  Actor1Type3Code varchar(3) ,
-  Actor2Code varchar(20) , --char(3)
-  Actor2Name varchar(255) ,
-  Actor2CountryCode varchar(3) ,
-  Actor2KnownGroupCode varchar(3) ,
-  Actor2EthnicCode varchar(3) ,
-  Actor2Religion1Code varchar(3) ,
-  Actor2Religion2Code varchar(3) ,
-  Actor2Type1Code varchar(3) ,
-  Actor2Type2Code varchar(3) ,
-  Actor2Type3Code varchar(3) ,
+  Actor1Code text , --char(3)
+  Actor1Name text ,
+  Actor1CountryCode text ,
+  Actor1KnownGroupCode text ,
+  Actor1EthnicCode text ,
+  Actor1Religion1Code text ,
+  Actor1Religion2Code text ,
+  Actor1Type1Code text ,
+  Actor1Type2Code text ,
+  Actor1Type3Code text ,
+  Actor2Code text , --char(3)
+  Actor2Name text ,
+  Actor2CountryCode text ,
+  Actor2KnownGroupCode text ,
+  Actor2EthnicCode text ,
+  Actor2Religion1Code text ,
+  Actor2Religion2Code text ,
+  Actor2Type1Code text ,
+  Actor2Type2Code text ,
+  Actor2Type3Code text ,
   IsRootEvent int ,
-  EventCode varchar(4) ,
-  EventBaseCode varchar(4) ,
-  EventRootCode varchar(4) ,
+  EventCode text ,
+  EventBaseCode text ,
+  EventRootCode text ,
   QuadClass int ,
   GoldsteinScale real ,
   NumMentions int ,
   NumSources int ,
   NumArticles int ,
   AvgTone real ,
-  Actor1Geo_Type varchar(20) , --int
-  Actor1Geo_FullName varchar(255) ,
-  Actor1Geo_CountryCode varchar(2) ,
-  Actor1Geo_ADM1Code varchar(4) ,
+  Actor1Geo_Type text , --int
+  Actor1Geo_FullName text ,
+  Actor1Geo_CountryCode text ,
+  Actor1Geo_ADM1Code text ,
   Actor1Geo_Lat float ,
   Actor1Geo_Long float ,
-  Actor1Geo_FeatureID varchar(20) , --int
-  Actor2Geo_Type varchar(20) , --int
-  Actor2Geo_FullName varchar(255) ,
-  Actor2Geo_CountryCode varchar(2) ,
-  Actor2Geo_ADM1Code varchar(4) ,
+  Actor1Geo_FeatureID text , --int
+  Actor2Geo_Type text , --int
+  Actor2Geo_FullName text ,
+  Actor2Geo_CountryCode text ,
+  Actor2Geo_ADM1Code text ,
   Actor2Geo_Lat float ,
   Actor2Geo_Long float ,
-  Actor2Geo_FeatureID varchar(20) , --int
-  ActionGeo_Type varchar(20) , --int
-  ActionGeo_FullName varchar(255) ,
-  ActionGeo_CountryCode varchar(2) ,
-  ActionGeo_ADM1Code varchar(4) ,
+  Actor2Geo_FeatureID text , --int
+  ActionGeo_Type text , --int
+  ActionGeo_FullName text ,
+  ActionGeo_CountryCode text ,
+  ActionGeo_ADM1Code text ,
   ActionGeo_Lat float ,
   ActionGeo_Long float ,
-  ActionGeo_FeatureID varchar(20) , --int
+  ActionGeo_FeatureID text , --int
   DATEADDED int ,
-  SOURCEURL varchar(255)
+  SOURCEURL text
 );
 
